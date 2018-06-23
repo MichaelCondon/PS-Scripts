f09ff13fc73dcbb01083b1a06f55cf6b004c6dc5
Michael C <michaelcondon@outlook.com>
2018-06-23 13:22:53 +1000

    Added DeliveryOptimisation to services

--- Update-Fix.ps1
+++ Update-Fix.ps1
@@ -1,12 +1,12 @@
 #This script stops the necessary services and deletes the software distribution folder and Delivery Optimisation to resolve windows update issues
 function checkServicesStatus(){ #checks whether the services are running and returns true if all are stopped
-    
     [bool]$result
     $bits = Get-Service BITS | Select-Object Status
     $wu = Get-Service wuauserv | Select-Object Status
     $uso = Get-Service UsoSvc | Select-Object Status
+    $dos = Get-Service DoSvc | Select-Object Status
 
-    if($bits -eq "Stopped" -and $wu -eq "Stopped" -and $uso -eq "Stopped"){
+    if($bits -eq "Stopped" -and $wu -eq "Stopped" -and $uso -eq "Stopped" -and $dos -eq "Stopped"){
         $result = $true
         return $result
     }
@@ -15,29 +15,26 @@ function checkServicesStatus(){ #checks whether the services are running and ret
         return $result
     }
 }
-
-
-
+#Stop the services required
 Stop-Service BITS -F
 
 Stop-Service wuauserv -F
 
 Stop-Service UsoSvc -F
 
+Stop-Service DoSvc -F
 
-Write-Host "Stopping BITS, Update Orchestrator and Update Services..."
-Start-Sleep 5
-
-
-# Write-Output Get-Service BITS | Select-Object Status
-# Write-Output Get-Service wuauserv | Select-Object Status
+Write-Host "Stopping BITS, Update Orchestrator, Delivery Optimisation, and Update Services..."
+Start-Sleep 5 #Wait just to make sure the services have completely shut off
 
 if(checkServicesStatus -eq $true){
     Remove-Item -Path c:\windows\SoftwareDistribution -Recurse -Force
-    Write-Host "SoftwareDistribution Successfully deleted"
+    Write-Host "SoftwareDistribution folder Successfully deleted"
+    #restart services to be nice
     Start-Service BITS
     Start-Service wuauserv 
     Start-Service UsoSvc
+    Start-Service DoSvc
 }
 
 
