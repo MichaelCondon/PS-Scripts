89df72c06fd87bf79353855e780a0cd93cab46a7
Michael <mr.mikiie@gmail.com>
2018-07-23 16:34:54 +1000

    Added extra information on privacy + added exit prompt

--- HIPB.ps1
+++ HIPB.ps1
@@ -1,8 +1,14 @@
-#This script uses Troy Hunt's "Have I Been Pwned API"
+
+
+#This script uses Troy Hunt's "Have I Been Pwned API, information can be found at https://haveibeenpwned.com/API/v2
+#I've written this to familiarise myself with how powershell handles basic web requests
+
 
 Write-Host "This tool uses Troy Hunt's 'Have I Been Pwned' API to check if your email has been included in any know data breach"
 Write-Host "The HIBP service is made publically available by Troy and the wonderful contributors at HIBP"
 Write-Host "To learn more about the service and check your accounts in depth, please visit https://haveibeenpwned.com/"
+Write-Host "As per the HIBP Privacy statement, the email you entered is only used to retrieve the address if it exists in the breach list, the address you search is never explicitly stored anywhere"
+
 Write-Host ""
 $email = Read-Host "Please enter the email address you would like to check"
 Write-Host ""
@@ -21,6 +27,7 @@ catch{
     $response = $_.Exception.Response.StatusCode.Value__
 }
 #If the response from the server was not a 404
+#This solution is a bit heavy handed, I'll figure this out soon
 if($response -ne "404"){
     Write-Host "Your email, $email has been found in the following breaches:" -ForegroundColor Red
     foreach($i in $response){
@@ -44,6 +51,6 @@ if($response -ne "404"){
 else{
  Write-Host "Your email, $email was not found in any known breaches." -ForegroundColor Green
 }
-
+Read-Host -Prompt "Press Enter to exit"
 
 
